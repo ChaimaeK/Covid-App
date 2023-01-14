ee39848792facdaa8d26deaffcba426dff0ab614
AbdelMarah <90782999+AbdelMarah@users.noreply.github.com>
2023-01-14 21:06:11 +0100

    Delete covid-api directory

--- covid-api/src/main/java/org/polytech/covid/Entities/Patient.java
+++
@@ -1,4 +0,0 @@
-package org.polytech.covid.Entities;
-
-public class Patient {
-}
--- covid-api/src/main/java/org/polytech/covid/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.polytech.covid.Repositories;
-
-import org.polytech.covid.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
